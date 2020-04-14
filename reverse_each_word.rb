3ad73444c59fc8b2bdf3b303d32266dd0f3fb02b
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-14 18:28:23 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+# def reverse_each_word(sentence)
+#   words = sentence.split(" ");
+#   reversed_words = [];
+#   words.each do |word|
+#     reversed_words << word.reverse
+#   end
+#   reversed_words.join(" ");
+# end
+
+def reverse_each_word(sentence)
+  words = sentence.split(" ");
+  reversed_words = [];
+  words.collect do |word| 
+    reversed_words << word.reverse
+  end
+  reversed_words.join(" ")
+end
