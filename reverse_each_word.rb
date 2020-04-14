4645106916cedbb4ff8367753680b47e0cc86d13
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-14 18:08:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word(sentence)
   words = sentence.split(" ");
+  reversed_words = [];
   words.each do |word|
-    word.reverse
+    reversed_words << word.reverse
   end
+  reversed_words;
 end
